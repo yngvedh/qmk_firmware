0024f7a0f633af581944f5e2382b85f3e0ebe22f
Yngve Devik Hammersland <yngve@me.com>
2019-08-19 23:10:44 +0200

    Include number row on the number layer

--- keyboards/planck/keymaps/yngvedh/keymap.c
+++ keyboards/planck/keymaps/yngvedh/keymap.c
@@ -1,5 +1,7 @@
 #include QMK_KEYBOARD_H
 
+#define V_____V _______
+
 #define QWERTY_ 0
 #define NUM_ 1
 #define MOVE_ 2
@@ -18,18 +20,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 		KC_LCTL, KC_LGUI, XXXXXXX, KC_LALT, L_NUM,       KC_SPC,       L_MOVE,  KC_RALT, XXXXXXX, L_FUNC,  KC_APP),
 	[NUM_] = LAYOUT_planck_1x2uC(
-		XXXXXXX, KC_EXLM, KC_PIPE, KC_PERC, KC_AMPR, XXXXXXX, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_P0,   XXXXXXX,
-		_______, KC_PLUS, KC_PMNS, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, XXXXXXX,
-		_______, KC_EQL,  KC_TILD, KC_QUOT, KC_AT,   XXXXXXX, KC_P0,   KC_P1,   KC_P2,   KC_P3,   XXXXXXX, _______,
-		_______, _______, _______, _______, _______,      _______,     XXXXXXX, KC_P0,   XXXXXXX, XXXXXXX, XXXXXXX),
+		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+		_______, KC_GRV,  KC_QUOT, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, XXXXXXX,
+		_______, XXXXXXX, KC_BSLS, KC_MINS, KC_EQL,  XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    XXXXXXX, _______,
+		_______, _______, _______, _______, V_____V,      _______,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX),
 	[MOVE_] = LAYOUT_planck_1x2uC(
-		XXXXXXX, KC_GRV,  KC_BSLS, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, KC_DEL,
-		_______, KC_ASTR, KC_UNDS, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
-		_______, KC_HASH, KC_CIRC, KC_DQUO, KC_DLR,  XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX, KC_PGDN, XXXXXXX, _______,
-		_______, _______, _______, _______, XXXXXXX,      _______,     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX),
+		_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, KC_DEL,
+		_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, XXXXXXX,
+		_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, _______,
+		_______, _______, _______, _______, XXXXXXX,      _______,     V_____V, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX),
 	[FUNC_] = LAYOUT_planck_1x2uC(
 		RESET,   DEBUG,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,
 		_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX,
 		_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
-		_______, _______, _______, _______, _______,      _______,     XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX)
+		_______, _______, _______, _______, XXXXXXX,      _______,     XXXXXXX, XXXXXXX, XXXXXXX, V_____V, XXXXXXX)
 };
+
+/*
+
+CHAR CODE SHIFT:
+- KC_MINS _  
+_ KC_UNDS
+" KC_QUOT '
+[ KC_LBRC {
+[ KC_LCBR {
+] KC_RBRC }
+] KC_RCBR }
+= KC_EQL  +
++ KC_PLUS
+| KC_BSLS \
+| KC_PIPE
+` KC_GRV ~
+~ KC_TILD
+) KC_RPRN
+! KC_EXLM
+@ KC_AT
+# KC_HASH
+$ KC_DLR 
+% KC_PERC
+^ KC_CIRC
+& KC_AMPR
+* KC_ASTR
+( KC_LPRN
+
+*/
