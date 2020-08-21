89fdd5a6727e1efad18c56a2d0280c82307da076
Yngve Devik Hammersland <yngve@me.com>
2020-08-21 20:24:18 +0200

    Add right OS key and remove muse.c from make

--- keyboards/planck/keymaps/yngvedh/keymap.c
+++ keyboards/planck/keymaps/yngvedh/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
 		CTL_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-		KC_LCTL, KC_LGUI, XXXXXXX, KC_LALT, L_NUM,       KC_SPC,       L_MOVE,  KC_RALT, XXXXXXX, L_FUNC,  KC_APP),
+		KC_LCTL, KC_LGUI, KC_RGUI, KC_LALT, L_NUM,       KC_SPC,       L_MOVE,  KC_RALT, XXXXXXX, L_FUNC,  KC_APP),
 	[NUM_] = LAYOUT_planck_1x2uC(
 		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
 		_______, KC_GRV,  KC_QUOT, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, XXXXXXX,
@@ -35,32 +35,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
 		_______, _______, _______, _______, XXXXXXX,      _______,     XXXXXXX, XXXXXXX, XXXXXXX, V_____V, XXXXXXX)
 };
-
-/*
-
-CHAR CODE SHIFT:
-- KC_MINS _  
-_ KC_UNDS
-" KC_QUOT '
-[ KC_LBRC {
-[ KC_LCBR {
-] KC_RBRC }
-] KC_RCBR }
-= KC_EQL  +
-+ KC_PLUS
-| KC_BSLS \
-| KC_PIPE
-` KC_GRV ~
-~ KC_TILD
-) KC_RPRN
-! KC_EXLM
-@ KC_AT
-# KC_HASH
-$ KC_DLR 
-% KC_PERC
-^ KC_CIRC
-& KC_AMPR
-* KC_ASTR
-( KC_LPRN
-
-*/
